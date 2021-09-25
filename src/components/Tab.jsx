f7f8cd7d998576c551a9a1036262326425864b4b
Terry Chan <cch01@protonmail.com>
2021-09-24 23:46:47 -0300

    feat: header tab buttons

---
+++ src/components/Tab.jsx
@@ -0,0 +1,12 @@
+import classnames from 'classnames'
+import React from 'react'
+
+const Tab = ({label, isOn}) => { 
+  return (
+    <div className={classnames({'border-b-4 ease-in-out': isOn}, "align-bottom w-16 p-1 pb-0 text-base")}>
+      {label}
+    </div>
+  )
+}
+
+export default Tab
