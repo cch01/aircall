7d5d8320a586ade43e5ce721e45efd3a913c22e6
Terry Chan <cch01@protonmail.com>
2021-09-26 02:35:45 -0300

    fix: 404 after refresh on production

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ const express = require('express');
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'dist')));
+
 app.set('port', process.env.PORT || 8080);
+
+app.get('*', (req, res) => res.sendfile(path.join(__dirname, 'dist/index.html')));
 
 const server = app.listen(app.get('port'), () => {
   console.log('listening on port ', server.address().port);
